8a450d996276137f3afe75b34a78a49fbf173c56
Yaroslav Slepukhin <YarslavSlepukhin@gmail.com>
2020-05-11 14:21:08 +0300

    add js for popup

--- source/js/app.js
+++ source/js/app.js
@@ -17,35 +17,4 @@ navMain.classList.remove('main-nav--opened');
 navMain.classList.add('main-nav--closed');
 
 
-// modal
-const FOCUSABLE_SELECTORS = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, *[tabindex], *[contenteditable]';
-const openModalBtn = document.querySelector('.open-popup');
-const closeModalBtn = document.querySelector('.popup');
-const modal = document.querySelector('.popup');
-const main = document.querySelector('main');
-const body = document.querySelector('body');
 
-function openModal() {
-  modal.style.display = 'flex';
-  body.style.overflow = 'hidden';
-  modal.querySelector(FOCUSABLE_SELECTORS).focus();
-  const focusableElements = main.querySelectorAll(FOCUSABLE_SELECTORS);
-  focusableElements.forEach(el => el.setAttribute('tabindex', '-1'));
-  modal.removeAttribute('aria-hidden');
-  main.setAttribute('aria-hidden', 'true');
-}
-
-function closeModal(event) {
-  if (event.target.className === 'popup') {
-    modal.style.display = 'none';
-    body.style.overflow = 'auto';
-    const focusableElements = main.querySelectorAll(FOCUSABLE_SELECTORS);
-    focusableElements.forEach(el => el.removeAttribute('tabindex'));
-    modal.setAttribute('aria-hidden', 'true');
-    main.removeAttribute('aria-hidden');
-    openModalBtn.focus();
-  }
-}
-
-openModalBtn.addEventListener('click', openModal);
-closeModalBtn.addEventListener('click', closeModal);
